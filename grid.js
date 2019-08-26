b5a4ba021c56ad744bee03e215a88b42b51b8b6f
lars.schnabel@gmx.de <lars.schnabel@gmx.de>
2019-08-26 11:07:54 +0200

    initial upload

---
+++ grid.js
@@ -0,0 +1,112 @@
+"use strict";
+//Globals
+let slide=0;
+let created = false;
+let max, former;
+
+//Laden der Bilder bei Seitenaufruf
+window.onload=()=>{
+    let boxList = document.querySelectorAll('.box');
+    for(let i=0; i<boxList.length; i++){
+        createImage(i, 'box');
+    }
+}
+
+//Erstellen der BildContainer für die Gallerie
+let createImage=(index)=>{
+    let box = document.getElementById('box-'+index);
+    let newImg= document.createElement('img');
+    newImg.setAttribute('src', 'img/pic'+index+'.jpg')
+    if(index <=3 || index >=6){        
+        newImg.setAttribute('width', '300px')
+    }else{
+        newImg.setAttribute('width', '605px')
+    }
+    newImg.setAttribute('height', '300px');
+    box.appendChild(newImg);        
+}
+
+//OnClick Function "Bild groß anzeigen"
+let zoomIn=(e)=>{
+    slide = e.target.src.charAt(52);
+    buildBorder();
+    initGalerie();
+}
+
+//Init Galerie
+let initGalerie=()=>{
+    document.getElementById('id01').style.display='block';
+    let display = document.getElementById('pic-display');
+    display.style.backgroundImage= "url('img/pic"+slide+".jpg')";
+    initMiniPics();
+}
+
+//Kleine Bilder initialisieren
+let initMiniPics=()=>{
+    let picList = document.querySelectorAll('.pics');
+    if(created === false){
+        for(let i=0; i<picList.length; i++){
+            getSmallImages(i);
+        }        
+    }
+    created = true;
+}
+
+let getSmallImages=(index)=>{
+    let pic = document.getElementById('pic-'+index);
+    let newImg = document.createElement('img');
+    newImg.setAttribute('src', 'img/pic'+index+'.jpg');
+    newImg.setAttribute('width', '100%');
+    newImg.setAttribute('height', '100%');
+    pic.appendChild(newImg);
+}
+
+//Bilder wechseln und Slideindex anpassen
+let switchPic=(index)=>{
+    let test = document.getElementById('pic-display');
+    test.style.backgroundImage= "url('img/pic"+index+".jpg')";
+    slide = index;
+    buildBorder();
+}
+
+//Roten Rahmen um das aktive Bild anzeigen
+let buildBorder=()=>{
+    enableActiveClass();
+    disableActiveClasee();
+    former = slide;    
+}   
+
+let enableActiveClass=()=>{
+    document.getElementById("box-"+slide).classList.toggle("active");
+    document.getElementById("pic-"+slide).classList.toggle("active");
+}
+
+let disableActiveClasee=()=>{
+    if(former != undefined){
+        document.getElementById("box-"+former).classList.toggle("active");
+        document.getElementById("pic-"+former).classList.toggle("active");
+    }
+}
+
+//Slider Arrows
+let nextSlide=()=>{
+    let display = document.getElementById('pic-display');    
+    if(slide <9){
+        slide++;        
+    }else{
+        slide=1;
+    }
+    display.style.backgroundImage= "url('img/pic"+slide+".jpg')";
+    buildBorder();
+}
+
+let formerSlide=()=>{
+    let display = document.getElementById('pic-display');
+    if(slide<1){
+        slide = 9;
+    }else{
+        slide--;        
+    }
+    display.style.backgroundImage= "url('img/pic"+slide+".jpg')";
+    buildBorder();
+}
